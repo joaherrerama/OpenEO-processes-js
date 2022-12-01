3956a6698bc3c269115a2510e743738cef9ab0f1
Jorge Andrés Herrera Maldonado <jorge@whatalocation.ai>
2022-12-01 01:54:52 +0100

    updating public path

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   publicPath: process.env.NODE_ENV === 'production'
-  ? '/OpenEO-processes-js'
+  ? '/OpenEO-processes-js/'
   : '/',
   plugins: [vue()],
   rollup: {
